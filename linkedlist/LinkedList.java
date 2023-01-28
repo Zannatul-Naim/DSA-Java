51ed9c656cc2c11829527b2649116abc9f8d656d
Zannatul-Naim <zannatulnaim09@gmail.com>
2023-01-28 21:28:50 +0600

    package updated

---
+++ linkedlist/LinkedList.java
@@ -0,0 +1,79 @@
+package linkedlist;
+
+class Node {
+  Node next;
+  int data;
+  
+  Node (int data) {
+    this.data = data;
+  }
+}
+
+
+public class LinkedList {
+  
+  Node head;
+
+  public void append(int data) {
+    if(head == null) {
+      head = new Node(data);
+      return;
+    }
+
+    Node current = head;
+    while(current.next != null) {
+      current = current.next;
+    }
+
+    current.next = new Node(data);
+  }
+
+  public void prepend(int data) {
+
+    Node newHead = new Node(data);
+    newHead.next = head;
+    head = newHead;
+
+  }
+
+  public void deleteWithValue(int data) {
+    
+    if(head == null) return;
+
+    if(head.data == data) {
+      head = head.next;
+    }
+
+    Node current = head;
+
+    while(current.next != null) {
+      if(current.next.data == data) {
+        current.next = current.next.next;
+      }
+      current = current.next;
+    }
+  }
+
+  public void display() {
+    if(head == null) return;
+
+    Node current = head;
+
+    while(current != null) {
+      System.out.print(current.data + " ");
+      current = current.next;
+    } System.out.println();
+  }
+
+  public static void main(String[] args) {
+    LinkedList list = new LinkedList();
+
+    for(int i = 1; i <= 10; i++) {
+      list.append(i);
+    }
+
+    list.display();
+    
+  }
+
+}
