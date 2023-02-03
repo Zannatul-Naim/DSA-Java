f6200d56fc92831a3fb4d4d018812d882c0acb57
Zannatul-Naim <zannatulnaim09@gmail.com>
2023-02-03 16:53:34 +0600

    Bubblesort optimized

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -6,6 +6,8 @@ public class BubbleSort {
 
         for(int i = 0; i < size-1; i++) {
 
+            boolean swapped = false;
+
             for(int j = 0; j < size-i-1; j++) {
                 if(arr[j] > arr[j+1]) {
                     
@@ -13,8 +15,14 @@ public class BubbleSort {
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
+
+                    swapped = true;
                     
                 }
+            }
+
+            if(!swapped) {
+                break;
             }
         }
     }
