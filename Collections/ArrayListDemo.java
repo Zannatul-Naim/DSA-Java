f4dd1c8835492ba3784b6660eb9afc6615b61fa8
Zannatul-Naim <zannatulnaim09@gmail.com>
2023-02-11 18:55:21 +0600

    Collections: ArrayList part added

---
+++ Collections/ArrayListDemo.java
@@ -0,0 +1,23 @@
+package Collections;
+
+import java.util.*;
+
+public class ArrayListDemo {
+    
+public static void main(String[] args) {
+    ArrayList <String> genre = new ArrayList <String> ();
+    
+    genre.add("mystery");
+    genre.add("thriller");
+    genre.add("fake");
+    genre.add("action");
+
+    // define an iterator for the list 
+    Iterator it = genre.iterator();
+
+    while(it.hasNext()) {
+        System.out.print(it.next() + "  ");
+    } System.out.println();
+
+}
+}
